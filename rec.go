df7c5dcb7fb29d7515752d78d8a5378542a6f5d1
Joshua Green <joshua.green@ericsson.com>
2018-02-27 14:59:20 +0100

    fixed deadlock issue
    the deadlock issue was caused by the URL backlog building up too much. I
    added an aggregator to remove the mutex on the result map, and increased
    the buffer size in the work channel. Otherwise, the workers get stuck
    trying to push the discovered URLs into the queue.

--- rec.go
+++ rec.go
@@ -10,8 +10,8 @@ import (
 )
 
 const (
-	NUM_WORKERS = 8
-	QUEUE_SIZE  = 30 + NUM_WORKERS*10
+	NUM_WORKERS = 32
+	QUEUE_SIZE  = NUM_WORKERS * 200
 )
 
 type Result struct {
@@ -23,10 +23,18 @@ type Result struct {
 	Note  string
 }
 
+type endpointData struct {
+	Url  *url.URL
+	Body string
+}
+
 func Rec(servRoot *url.URL, fname string, client ReqFunc, note string) error {
-	queue := make(chan *url.URL, QUEUE_SIZE)
+	worker_input := make(chan *url.URL, QUEUE_SIZE)
+	agg_url_input := make(chan *url.URL, QUEUE_SIZE)
+	agg_body_input := make(chan endpointData, QUEUE_SIZE)
 	stop := make(chan struct{})
-	pending := &sync.WaitGroup{}
+	var pending sync.WaitGroup
+
 	result := &Result{
 		State: make(map[string]string),
 		Date:  time.Now(),
@@ -35,48 +43,80 @@ func Rec(servRoot *url.URL, fname string, client ReqFunc, note string) error {
 		Note:  note,
 	}
 
+	// send the first url
 	pending.Add(1)
-	queue <- servRoot
+	agg_url_input <- servRoot
 
-	for i := 0; i < NUM_WORKERS; i++ {
-		go Worker(queue, stop, pending, client, result)
-	}
-	pending.Wait()
-
-	for i := 0; i < NUM_WORKERS; i++ {
-		stop <- struct{}{}
-	}
-	log.Printf("captured %d endpoints", len(result.State))
-	return DumpToFile(result, fname)
-}
-
-func Worker(queue chan *url.URL, stop chan struct{}, wg *sync.WaitGroup, client ReqFunc, result *Result) {
-	for {
-		select {
-		case <-stop:
-			return
-		case nextUrl := <-queue:
-			body, urls, err := Process(nextUrl, client)
-			if err != nil {
-				log.Print(err)
-				wg.Add(-1)
-				continue
-			}
-			result.Lock()
-			result.State[nextUrl.Path] = string(body)
-			result.Unlock()
-			for _, u := range urls {
-				result.RLock()
+	// aggregator
+	go func() {
+		for {
+			select {
+			case <-stop:
+				log.Print("agg stopping")
+				return
+			case u := <-agg_url_input:
+				log.Printf("agg got new url %s", u)
 				_, ok := result.State[u.Path]
-				result.RUnlock()
 				if !ok {
-					wg.Add(1)
-					queue <- u
+					log.Printf("agg sending url %s to workers", u)
+					worker_input <- u
+				} else {
+					log.Printf("agg ignoring url %s", u)
+					pending.Add(-1)
+				}
+			case ep := <-agg_body_input:
+				log.Printf("agg storing url %s", ep.Url)
+				result.State[ep.Url.Path] = ep.Body
+			}
+		}
+		log.Print("agg fell out of loop")
+	}()
+
+	// workers
+	for i := 0; i < NUM_WORKERS; i++ {
+		go func(id int) {
+			for {
+				select {
+				case <-stop:
+					log.Printf("worker%d stopping", id)
+					return
+				case u := <-worker_input:
+					log.Printf("worker%d processing url %s", id, u)
+					body, urls, err := Process(u, client)
+					if err != nil {
+						log.Printf("worker%d error processing %s: %s", id, u, err)
+						pending.Add(-1)
+						continue
+					}
+
+					// report result to aggregator
+					log.Printf("worker%d sending body %s to aggregator", id, u)
+					agg_body_input <- endpointData{Url: u, Body: string(body)}
+
+					// send found urls to aggregator
+					for _, u := range urls {
+						log.Printf("worker%d sending new url %s to aggregator", id, u)
+						pending.Add(1)
+						agg_url_input <- u
+					}
+
+					// report we are done
+					log.Printf("worker%d done with %s", id, u)
+					pending.Add(-1)
 				}
 			}
-			wg.Add(-1)
-		}
+			log.Printf("worker%d fell out of loop", id)
+		}(i)
 	}
+
+	// wait until there are zero pending urls
+	pending.Wait()
+
+	// tell the aggregator and workers to stop
+	close(stop)
+
+	log.Printf("captured %d endpoints", len(result.State))
+	return DumpToFile(result, fname)
 }
 
 func DumpToFile(data *Result, fname string) error {
